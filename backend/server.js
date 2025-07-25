782ebea947501d206a18bbf1d61b2ef99736513a
Aaditya <aadityadahiya1617@gmail.com>
2025-07-25 14:55:32 +0530

    frontend url added

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(
   '/uploads',
   express.static(path.join(__dirname, 'uploads'), {
     setHeaders: (res, _path) => {
-      res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.set('Access-Control-Allow-Origin', 'https://resumebuilder-frontend-m558.onrender.com');
     },
   })
 );
